07f8ff022630b2ec030d1746eed4f346825a9a8b
olegaver-xxx <oleg.bakiev.jrs@gmail.com>
2025-10-06 23:05:56 +0300

    views update

--- main/views.py
+++ main/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 
 from main.models import Post
 
 
-# Create your views here.
-class MainView(ListView):
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+class PostsView(ListView):
     model = Post
     template_name = 'index.html'
     context_object_name = 'posts'
+
+
+class DetailPostView(DetailView):
+    model = Post
+    template_name = ...
